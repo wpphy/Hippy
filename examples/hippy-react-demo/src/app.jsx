ae91cf67d12adec1174d23e5e227b39dcfc6f44d
pointpeng <pointpeng@tencent.com>
2023-11-01 15:28:55 +0800

    feat(example): iOS Listview accessible DEMO

--- examples/hippy-react-demo/src/app.jsx
+++ examples/hippy-react-demo/src/app.jsx
@@ -1,92 +1,74 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   View,
   Text,
-  Platform,
+  ListView,
 } from '@hippy/react';
-import HomeEntry from './pages/entry';
-import Debug from './pages/debug';
 import SafeAreaView from './shared/SafeAreaView';
+import style from './style';
 
-const styles = StyleSheet.create({
-  buttonContainer: {
-    height: 48,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-  },
-  button: {
-    height: 48,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    borderTopWidth: 1,
-    borderTopColor: '#eee',
-  },
-  buttonText: {
-    color: '#242424',
-    fontSize: 16,
-  },
-  blankPage: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-});
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = ({
-      pageIndex: 0,
-    });
+  }
+
+  getRowType(item) {
+    return item.rowType || 0;
+  }
+
+  getRowKey(index) {
+    return index;
+  }
+
+  clickCb() {
+    console.log('click');
+  }
+
+
+  renderRow(item) {
+    let result = '';
+    switch (item.rowType) {
+      case 1:
+        result = (
+          <View style={style.section_tit}>
+            <View style={style.section_tit_left}>
+              <Text style={[style.section_tit_txt]}>歌单达人投稿周排行</Text>
+              <View onClick={this.clickCb.bind(this)} accessible={true} accessibilityLabel={'歌单达人投稿周排行规则按钮'}>
+                <Text style={{ color: 'green' }}>规则</Text>
+              </View>
+            </View>
+            <View
+              style={style.section_tit_right}
+              onClick={this.clickCb.bind(this)}
+              accessible={true}
+              accessibilityLabel={'更多按钮'}
+            >
+              <Text style={[style.section_tit_more]}>更多</Text>
+
+            </View>
+          </View>
+        );
+        break;
+      default:
+        break;
+    }
+    return result;
   }
 
   render() {
-    const { pageIndex } = this.state;
-    const { isSimulator, __instanceId__: instanceId } = this.props;
-
-    const renderPage = () => {
-      switch (pageIndex) {
-        case 0:
-          return <HomeEntry />;
-        case 1:
-        case 2:
-          return <Debug instanceId={instanceId} />;
-        default:
-          return <View style={styles.blankPage} />;
-      }
-    };
-
-    const renderButton = () => {
-      let buttonArray = ['API', '本地调试'];
-      // iOS 真机仅支持查看范例
-      if (Platform.OS === 'ios' && !isSimulator) {
-        buttonArray = ['API'];
-      }
-      return (
-        buttonArray.map((v, i) => (
-          <View
-            key={`button_${v}`}
-            style={styles.button}
-            onClick={() => this.setState({ pageIndex: i })}
-          >
-            <Text
-              style={[styles.buttonText, i === pageIndex ? { color: '#4c9afa' } : null]}
-              numberOfLines={1}
-            >
-              {v}
-            </Text>
-          </View>
-        ))
-      );
-    };
-
+    const list = [{ rowType: 1 }, { rowType: 1 }];
     return (
       <SafeAreaView statusBarColor="#4c9afa">
-        {renderPage()}
-        <View style={styles.buttonContainer}>
-          {renderButton()}
+        <View style={[style.container]}>
+          <ListView
+            style={{ flex: 1 }}
+            numberOfRows={list.length}
+            renderRow={index => this.renderRow(list[index])}
+            getRowType={index => this.getRowType(list[index])}
+            getRowKey={index => this.getRowKey(index)}
+            showScrollIndicator={false}
+          />
         </View>
       </SafeAreaView>
     );
---
+++ examples/hippy-react-demo/src/style.js
@@ -0,0 +1,59 @@
+export default {
+  container: {
+    paddingHorizontal: 16,
+    paddingTop: 300,
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  section_tit: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 14,
+    marginBottom: 4,
+  },
+  section_tit_left: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  section_tit_txt: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  section_tit_info_icon: {
+    width: 14,
+    height: 14,
+    marginLeft: 5,
+  },
+  section_tit_right: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  section_tit_more: {
+    fontSize: 12,
+    lineHeight: 14,
+    color: 'green',
+  },
+  section_tit_icon: {
+    width: 12,
+    height: 13,
+  },
+  section_bot: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 0,
+    height: 40,
+    marginBottom: 30,
+  },
+  section_bot_txt: {
+    fontSize: 14,
+    lineHeight: 16,
+  },
+  section_bot_icon: {
+    width: 14,
+    height: 15,
+    top: 1,
+  },
+};
